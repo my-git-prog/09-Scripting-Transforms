9a75263f9d621ebefbb69e331f4771e9c242af88
my-git-prog <pavel.serpukhov@gmail.com>
2024-11-29 21:43:09 +0300

    Uploading

---
+++ Assets/Scripts/AroundYRotator.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AroundYRotator : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    
+    private void Update()
+    {
+        transform.Rotate(Vector3.up, _speed * Time.deltaTime, Space.Self);
+    }
+}
---
+++ Assets/Scripts/ScaleEnlarger.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleEnlarger : MonoBehaviour
+{
+    private const float EqualScaleMultiplier = 1.0f;
+
+    [SerializeField] private float _speed;
+
+    private void Update()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale *= (EqualScaleMultiplier + _speed * Time.deltaTime);
+        transform.localScale = localScale;
+    }
+}
